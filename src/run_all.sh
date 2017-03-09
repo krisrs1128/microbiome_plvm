6d6031a56dde6331c2d23672fe13e5947d2a851d
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-08 21:50:20 -0800

    Run simulation for LDA

--- src/run_all.sh
+++ src/run_all.sh
@@ -2,4 +2,10 @@ source .env
 
 ## Runs simulation and generates figures for the NMF experiment
 #Rscript $MICROBIOME_PLVM_DIR/src/sim/nmf/nmf_expers.R
-Rscript $MICROBIOME_PLVM_DIR/src/sim/nmf/nmf_vis.R
+#Rscript $MICROBIOME_PLVM_DIR/src/sim/nmf/nmf_vis.R
+
+cd $MICROBIOME_PLVM_DIR/src/sim/lda/pipeline
+pwd
+Rscript ../Rscript/build_config.R
+python3 pipeline.py LDAExperiment --local-scheduler --workers=8
+cd $MICROBIOME_PLVM_DIR
