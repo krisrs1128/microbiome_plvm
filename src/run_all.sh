d991b7d429c688030f8a3795c8f303a412eaf24f
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-15 23:08:24 -0700

    uncomment

--- src/run_all.sh
+++ src/run_all.sh
@@ -8,14 +8,14 @@
 source .env
 
 ## Runs simulation and generates figures for the NMF experiment
-# Rscript $MICROBIOME_PLVM_DIR/src/sim/nmf/nmf_expers.R
-# Rscript $MICROBIOME_PLVM_DIR/src/sim/nmf/nmf_vis.R
+Rscript $MICROBIOME_PLVM_DIR/src/sim/nmf/nmf_expers.R
+Rscript $MICROBIOME_PLVM_DIR/src/sim/nmf/nmf_vis.R
 
 ## Run simulations and generate figures for LDA experiment
 # cd $MICROBIOME_PLVM_DIR/src/sim/lda/pipeline
-# Rscript ../Rscript/build_config.R
-# python3 pipeline.py LDAExperiment --local-scheduler --workers=2
-# Rscript ../lda_vis.R
+Rscript ../Rscript/build_config.R
+python3 pipeline.py LDAExperiment --local-scheduler --workers=2
+Rscript ../lda_vis.R
 
 cd $MICROBIOME_PLVM_DIR
 
