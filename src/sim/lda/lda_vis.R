b567a4c684fe33d53aac44e5674f05a060c69710
Kris Sankaran <sankaran.kris@gmail.com>
2017-05-04 11:06:50 -0700

    use square root scale in lda plot

--- src/sim/lda/lda_vis.R
+++ src/sim/lda/lda_vis.R
@@ -18,7 +18,8 @@ library("ggplot2")
 library("ldaSim")
 
 ## ---- paths ----
-base_dir <- Sys.getenv("MICROBIOME_PLVM_DIR")
+#base_dir <- Sys.getenv("MICROBIOME_PLVM_DIR")
+base_dir <- "~/Desktop/microbiome_plvm"
 lda_dir <- file.path(base_dir, "src", "sim", "lda")
 output_path <- file.path(lda_dir, "fits")
 metadata <- read_csv(file.path(output_path, "metadata.csv")) %>%
@@ -57,7 +58,7 @@ for (i in seq_along(unique_V)) {
 ## ---- betacontours1 ----
 p[[1]] <- p[[1]] +
   labs(x = expression(sqrt(hat(beta)[1])), y = expression(sqrt(hat(beta)[2]))) +
-  facet_grid(N + D ~ method)
+  facet_grid(method ~ D + N)
 
 ggsave(
   file.path(base_dir, "doc", "figure/betacontours1-1.pdf"),
@@ -68,22 +69,24 @@ ggsave(
 
 ## ---- relative-errors ----
 perf <- combined %>%
-  mutate(value_1 = log(value_1), truth_1 = log(truth_1), value_2 = log(value_2), truth_2 = log(truth_2)) %>%
+  mutate(estimate_1 = sqrt(estimate_1), truth_1 = sqrt(truth_1), estimate_2 = sqrt(estimate_2), truth_2 = sqrt(truth_2)) %>%
   group_by(variable, method, D, V, N) %>%
   summarise(
-    error = mean(sqrt((value_1 - truth_1) ^ 2 + (value_2 - truth_2) ^ 2)),
-    error_bar = sd(value_1)
+    error = mean(sqrt((estimate_1 - truth_1) ^ 2 + (estimate_2 - truth_2) ^ 2)),
+    error_bar = sd(estimate_1)
   )
 
 theme_set(ggscaffold::min_theme(list(border_size = .7)))
 method_cols <- c("#ae7664", "#64ae76", "#7664ae")
 p <- ggplot(perf) +
   geom_abline(slope = 1, alpha = 0.6, size = 0.3) +
-  geom_point(aes(x = log(error), y = log(error_bar), col = method), size = 0.7, alpha = 0.6) +
+  geom_point(aes(x = error, y = error_bar, col = method), size = 0.7, alpha = 0.6) +
   scale_color_manual(values = method_cols) +
+  scale_x_log10(breaks = c(0.01, 0.05, 0.1)) +
+  scale_y_log10(breaks = c(0.01, 0.05, 0.1)) +
   guides(color = guide_legend(override.aes = list(alpha = 1, size = 2))) +
   labs(x = "Error", y = "SD (k = 1)") +
-  facet_grid(N ~ V + D)
+  facet_grid(V ~ D + N)
 
 ggsave(
   file.path(base_dir, "doc", "figure/beta_errors_lda.pdf"),
