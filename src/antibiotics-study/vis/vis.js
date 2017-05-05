8e4580665b3ca36d2e3909a19eee1d0ea43bcec1
Kris Sankaran <sankaran.kris@gmail.com>
2017-05-04 19:13:37 -0700

    voronoi mouseovers

--- src/antibiotics-study/vis/vis.js
+++ src/antibiotics-study/vis/vis.js
@@ -47,23 +47,68 @@ elem.selectAll("circle")
   .enter()
   .append("circle")
   .attrs({
+    "class": function(d) {
+      var class_text = "circle-" + d.ix + "-" + d.topic;
+      return class_text.replace(/\s/g, "-");
+    },
     "r": 1,
     "cx": function(d) { return scales.x(d.ix); },
     "cy": function(d) { return scales.panels(d.topic) + scales.y(d.median); },
     "fill": function(d) { return scales.fill(d.fill); }
+  });
+
+var voronoi = d3.voronoi()
+    .x(function(d) { return scales.x(d.ix); })
+    .y(function(d) { return scales.panels(d.topic) + scales.y(d.median); })
+    .extent([[0, 0], [width, height]]);
+
+var poly = voronoi(beta)
+    .polygons();
+poly = poly.filter(function(d) { return !typeof(d) != "undefined"; });
+
+elem.selectAll("path")
+	.data(poly)
+	.enter()
+  .append("path")
+  .attr("d", function(d, i) { return "M" + d.join("L") + "Z"; })
+  .attrs({
+    "class": function(d, i) { return "voronoi" + d.data.ix + "-" + d.data.topic; },
+    "fill": "none",
+    "pointer-events": "all"
   })
-  .on("mouseover", update_info);
+	.on("mouseover", info_over)
+	.on("mouseout", info_out);
 
-function update_info(d) {
-  d3.select("#text_box")
-    .selectAll("text")
-    .remove()
-
+function info_over(d) {
   d3.select("#text_box")
     .append("text")
-    .text(d.ix)
+    .text(d.data.ix)
     .attrs({
       "transform": "translate(10, 20)",
       "font-size": 20
+    });
+
+  var select_text = ".circle-" + d.data.ix + "-" + d.data.topic;
+  select_text = select_text.replace(/\s/g, "-");
+  d3.select(select_text)
+    .transition()
+    .duration(100)
+    .attrs({
+      "r": 4
+    });
+}
+
+function info_out(d) {
+  d3.select("#text_box")
+    .selectAll("text")
+    .remove();
+
+  var select_text = ".circle-" + d.data.ix + "-" + d.data.topic;
+  select_text = select_text.replace(/\s/g, "-");
+  d3.select(select_text)
+    .transition()
+    .duration(100)
+    .attrs({
+      "r": 1
     });
 }
