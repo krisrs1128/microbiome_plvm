cccb3da3ff2eef031d541ac7315338bf1eb1c22f
Kris Sankaran <sankaran.kris@gmail.com>
2017-05-07 16:16:10 -0700

    no longer compute hists
    these are never included in the paper

--- src/antibiotics-study/posterior_checks.R
+++ src/antibiotics-study/posterior_checks.R
@@ -49,13 +49,12 @@ for (i in seq_along(data_types)) {
   merged_data[[data_types[i]]] <- do.call(rbind, cur_data)
 }
 
-# ---- plot ----
+## ---- plot ----
 p <- posterior_checks_plots(merged_data)
 
 output_dir <- "../../doc/figure/"
 dir.create(output_dir, recursive = TRUE)
 
-ggsave(sprintf("%s/posterior_check_hists-%s.png", output_dir, argv$subject), p[["hists"]], width = 6, height = 3.5)
 ggsave(sprintf("%s/posterior_check_quantiles-%s.png", output_dir, argv$subject), p[["quantiles"]], width = 4.5, height = 2.2)
 ggsave(sprintf("%s/posterior_check_margins-%s.png", output_dir, argv$subject), p[["margins"]], width = 6, height = 3.5)
 ggsave(sprintf("%s/posterior_check_ts-%s.png", output_dir, argv$subject), p[["ts"]], width = 6, height = 4)
