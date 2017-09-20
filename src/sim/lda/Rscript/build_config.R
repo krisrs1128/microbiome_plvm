f26d94fef6bb1413b861899ae394a65046d42d4c
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-20 10:43:24 -0700

    update lda simulation parameters based on review

--- src/sim/lda/Rscript/build_config.R
+++ src/sim/lda/Rscript/build_config.R
@@ -1,9 +1,9 @@
 library("jsonlite")
 df <- expand.grid(
   D = c(20, 100),
-  V = c(10, 50),
+  V = c(750, 1500),
   K = 2,
-  N = c(20, 50, 100),
+  N = c(3750, 7500, 15000),
   alpha0 = 1,
   gamma0 = 1
 )
