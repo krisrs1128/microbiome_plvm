56a2004a4e042ccf30dd6649f84a7e84d988e1c3
Kris Sankaran <sankaran.kris@gmail.com>
2017-10-22 16:24:58 -0700

    revising plotting in nmf

--- src/sim/nmf/nmf_vis.R
+++ src/sim/nmf/nmf_vis.R
@@ -24,13 +24,18 @@ dir.create(figure_dir, recursive = TRUE)
 
 fits <- list.files(fits_dir, "fit-*", full.names = TRUE)
 expers <- fromJSON(
-  file.path(nmf_dir, "config.json"),
+  ## file.path(nmf_dir, "config.json"),
+  "~/Desktop/src/sim/nmf/config.json",
   simplifyVector = FALSE
 )
 
+exper_ids <- paste0(sapply(expers, function(x) x$id), ".rda")
+expers <- expers[exper_ids %in% basename(fits)]
+
 beta_fits <- reshape_all_samples(
   fits,
-  file.path(nmf_dir, "config.json"),
+  ## file.path(nmf_dir, "config.json"),
+  "~/Desktop/src/sim/nmf/config.json",
   "beta",
   c("j", "k")
 ) %>%
@@ -84,7 +89,7 @@ plot_opts$group <- "i"
 zinf_data$zero_inf_prob <- as.character(zinf_data$zero_inf_prob)
 zinf_data <- zinf_data %>%
   mutate(
-    EN = paste0("E[N] = ", P * b * 10 / as.numeric(a))
+    EN = paste0("E[N] = ", P * b * 20 / as.numeric(a))
   )
 
 perf <- zinf_data %>%
@@ -127,33 +132,58 @@ ggsave(
 
 ## ---- zinf-betas-contours ----
 combined <- zinf_data %>%
-  filter(P == 10)
+  filter(P == 325)
+combined <- combined[sample(1:nrow(combined), 500), ]
+
+  filter(method == "bootstrap") %>%
+  rename(truth_2 = truth_1, truth_1 = truth_2, value_1 = value_2, value_2 = value_1)
+
+posterior_means <- combined %>%
+  group_by(j, D, a, b, zero_inf_prob, inference, method) %>%
+  summarise(
+    value_mean_1 = mean(value_1),
+    value_mean_2 = mean(value_2),
+    truth_1 = truth_1[1],
+    truth_2 = truth_2[1]
+  )
 
 plot_opts <- list(x = "sqrt(value_1)", y = "sqrt(value_2)",
-                  group = "j", fill_type = "gradient", h = 0.1,
+                  group = "j", fill_type = "gradient", h = 1, # was h = 0.1
                   theme_opts = list(border_size = 0.7))
 
 p <- ggcontours(combined, plot_opts) +
-  geom_text(
-    data = combined %>%
-      filter(iteration == 1),
+  geom_point(
+    data = posterior_means,
     aes(
       x = sqrt(truth_1),
-      y = sqrt(truth_2),
-      label = j
+      y = sqrt(truth_2)
     ),
-    size = 2
+    size = 0.5,
+    alpha = 0.4
   ) +
-  geom_text(
-    data = combined %>%
-      group_by(j, D, a, b, zero_inf_prob, inference, method) %>%
-      summarise(value_mean_1 = mean(value_1), value_mean_2 = mean(value_2)),
+  ylim(0, 3) +
+  xlim(0, 4.5) +
+  geom_point(
+    data = posterior_means,
+    aes(
+      x = sqrt(value_mean_1),
+      y = sqrt(value_mean_2)
+    ),
+    size = 0.5,
+    alpha = 0.4,
+    col = "#fc8d62"
+  ) +
+  geom_segment(
+    data = posterior_means,
     aes(
       x = sqrt(value_mean_1),
       y = sqrt(value_mean_2),
-      label = j
+      xend = sqrt(truth_1),
+      yend = sqrt(truth_2)
     ),
-    size = 2, col = "#fc8d62") +
+    size = 0.5,
+    alpha = 0.5
+  ) +
   ylim(0, 3) +
   xlim(0, 4.5) +
   facet_grid(method + inference + zero_inf_prob ~ D + EN) +
