d468405d692c0ffc204751d7e682a5e6cb734f1c
Kris Sankaran <sankaran.kris@gmail.com>
2019-12-29 20:52:06 -0500

    approach with explicitly held out species

--- src/antibiotics-study/lda.R
+++ src/antibiotics-study/lda.R
@@ -9,14 +9,16 @@
 library("argparser")
 parser <- arg_parser("Perform LDA on the antibiotics dataset")
 parser <- add_argument(parser, "--subject", help = "Subject on which to perform analysis", default = "F")
-parser <- add_argument(parser, "--K", help = "Number of LDA topics", default = 4)
+parser <- add_argument(parser, "--K", help = "Number of LDA topics", default = 2)
 argv <- parse_args(parser)
 
 ## ---- setup ----
 library("rstan")
 library("reshape2")
-library("tidyverse")
 library("stringr")
+library("tidyr")
+library("dplyr")
+library("tibble")
 library("phyloseq")
 library("feather")
 theme_set(theme_bw(base_size = 7))
@@ -25,6 +27,7 @@ dir.create("../../data/fits/", recursive = TRUE)
 dir.create("../../data/figure-input/", recursive = TRUE)
 dir.create("../../doc/figure/", recursive = TRUE)
 set.seed(11242016)
+options(mc.cores = parallel::detectCores())
 
 ## ---- get-data ----
 abt <- get(load("../../data/antibiotics-study/abt.rda"))
@@ -44,6 +47,7 @@ releveled_sample_data <- abt %>%
       "Post Cp" = "Post"
     )
   )
+
 rownames(releveled_sample_data) <- abt %>%
   sample_data %>%
   rownames
@@ -69,14 +73,16 @@ y_order <- names(sort(sample_sums(abt)))
 
 ## ---- lda ----
 x <- t(get_taxa(abt))
+out_ix <- c(160, 343, 891, 1036, 1045, 1086, 1116, 1131, 1417, 1463, 1659, 1895)
+
 dimnames(x) <- NULL
 stan_data <- list(
   K = argv$K,
-  V = ncol(x),
+  V = ncol(x[, -out_ix]),
   D = nrow(x),
-  n = x,
+  n = x[, -out_ix],
   alpha = rep(1, argv$K),
-  gamma = rep(0.5, ncol(x))
+  gamma = rep(0.3, ncol(x[, -out_ix]))
 )
 
 start_fit <- Sys.time()
@@ -85,7 +91,7 @@ stan_fit <- vb(
   f,
   data = stan_data,
   output_samples = 1000,
-  eta = 1,
+  eta = 0.1,
   adapt_engaged = FALSE
 )
 cat(sprintf(
@@ -105,310 +111,22 @@ save(
 samples <- rstan::extract(stan_fit)
 rm(stan_fit)
 
-## ---- extract_beta ----
-# underlying RSV distributions
-beta_logit <- samples$beta
+## ---- simulate-holdout ----
+thetai <- samples$theta[1,,]
+x_out <- x[, out_ix]
 
-for (i in seq_len(nrow(beta_logit))) {
-  for (k in seq_len(stan_data$K)) {
-    beta_logit[i, k, ] <- log(beta_logit[i, k, ])
-    beta_logit[i, k, ] <- beta_logit[i, k, ] - mean(beta_logit[i, k, ])
-  }
+B <- nrow(samples$theta)
+lx_hat <- matrix(nrow=B, ncol=nrow(x_out))
+for (i in seq_len(B)) {
+  lx_hat[i, ] <- predict(lm(asinh(x_out[, 2]) ~ samples$theta[i,,]))
 }
 
-beta_hat <- beta_logit %>%
-  melt(
-    varnames = c("iterations", "topic", "rsv_ix"),
-    value.name = "beta_logit"
-  ) %>%
-  as_data_frame()
-
-beta_hat$rsv <- rownames(tax_table(abt))[beta_hat$rsv_ix]
-taxa <- as_data_frame(tax_table(abt)@.Data)
-taxa$rsv <- rownames(tax_table(abt))
-taxa$Taxon_5[which(taxa$Taxon_5 == "")] <- taxa$Taxon_4[which(taxa$Taxon_5 == "")]
-
-beta_hat <- beta_hat %>%
-  left_join(taxa) %>%
-  mutate(
-    topic = paste("Topic", topic),
-    Taxon_5 = str_extract(Taxon_5, "[^_]+")
-  )
-
-sorted_taxa <- names(sort(table(beta_hat$Taxon_5), decreasing = TRUE))
-beta_hat$Taxon_5 <- factor(beta_hat$Taxon_5, levels = sorted_taxa)
-beta_hat$rsv <- factor(beta_hat$rsv, levels = taxa$rsv)
-
-## ---- extract_theta ----
-theta_logit <- samples$theta
-for (i in seq_len(nrow(theta_logit))) {
-  for (d in seq_len(stan_data$D)) {
-    theta_logit[i, d, ] <- log(theta_logit[i, d, ])
-    theta_logit[i, d, ] <- theta_logit[i, d, ] - mean(theta_logit[i, d, ])
-  }
+plot(asinh(x_out[, 7]), type="l")
+for (i in seq_len(B)) {
+  points(lx_hat[i, ], col=rgb(0, 0, 0, 0.1))
 }
 
-theta_hat <- theta_logit %>%
-  melt(
-    varnames = c("iteration", "sample", "topic"),
-    value.name = "theta_logit"
-  )
-
-theta_hat$sample <- sample_names(abt)[theta_hat$sample]
-sample_info <- data.frame(sample_data(abt))
-sample_info$sample <- rownames(sample_info)
-theta_hat$topic <- paste("Topic", theta_hat$topic)
-
-theta_hat <- theta_hat %>%
-  left_join(sample_info, by = "sample")
-
-## ---- visualize_lda_theta_heatmap ----
-plot_opts <- list(
-  "x" = "time",
-  "y" = "topic",
-  "fill" = "mean_theta",
-  "y_order" = paste("Topic", stan_data$K:1)
-)
-
-## ---- visualize_lda_theta_boxplot ----
-p <- ggplot(theta_hat) +
-  geom_boxplot(
-    aes(x = as.factor(time), y = theta_logit),
-    fill = "#C9C9C9",
-    outlier.size = 0.05,
-    size = 0.1,
-    notchwidth = 0.1,
-    position = position_dodge(width = 0)
-  ) +
-  scale_y_continuous(breaks = scales::pretty_breaks(3)) +
-  facet_grid(topic ~ condition, scales = "free_x", space = "free_x") +
-  geom_hline(yintercept = 0, alpha = 0.4, size = 0.5, col = "#999999") +
-  labs(x = "Time", y = expression(paste("g(", theta[k], ")"))) +
-  theme(legend.position = "none") +
-  scale_x_discrete(breaks = seq(1, 60, by = 10) - 1)
-ggsave(
-  sprintf("../../doc/figure/visualize_lda_theta_boxplot-%s-%s.png", argv$subject, argv$K),
-  p, width = 6, height = 3.3
-)
-
-## ---- visualize_lda_beta ----
-beta_summary <- beta_hat %>%
-  group_by(rsv_ix, topic) %>%
-  summarise(
-    rsv = rsv[1],
-    beta_median = median(beta_logit),
-    Taxon_5 = Taxon_5[1],
-    beta_upper = quantile(beta_logit, 0.975),
-    beta_lower = quantile(beta_logit, 0.025)
-  )
-
-levels(beta_summary$Taxon_5) <- append(levels(beta_summary$Taxon_5), "other")
-beta_summary$Taxon_5[!(beta_summary$Taxon_5 %in% levels(beta_summary$Taxon_5)[1:7])] <- "other"
-
-beta_subset <- beta_summary %>%
-  filter(rsv %in% rev(x_order)[1:750])
-beta_subset$rsv_ix <- rep(seq_len(nrow(beta_subset) / 4), each = 4)
-
-p <- ggplot(beta_subset) +
-  geom_hline(yintercept = 0, alpha = 0.4, size = 0.5, col = "#999999") +
-  geom_point(aes(x = rsv_ix, y = beta_median, col = Taxon_5), size = 0.1) +
-  geom_errorbar(
-    aes(x = rsv_ix, alpha = beta_upper, ymax = beta_upper, ymin = beta_lower, col = Taxon_5),
-    size = 0.4
-  ) +
-  scale_color_brewer(palette = "Set2") +
-  scale_alpha(range = c(0.01, 1), breaks = c(1, 2, 3), guide = FALSE) + ## larger values have darker CIs
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(breaks = scales::pretty_breaks(3), limits = c(-5, 12)) +
-  facet_grid(topic ~ .) +
-  guides(color = guide_legend(override.aes = list(alpha = 1, size = 2))) +
-  labs(x = "Species", y = expression(paste("g(", beta[k], ")")), col = "Family") +
-  theme(
-    panel.border = element_rect(fill = "transparent", size = 0.75),
-    axis.text.x = element_blank(),
-    strip.text.x = element_blank(),
-    legend.position = "bottom"
-  )
-ggsave(
-  sprintf("../../doc/figure/visualize_lda_beta-%s-%s.png", argv$subject, argv$K),
-  p, width = 6, height = 3.5
-)
-
-## ---- beta-commentary ----
-inv_logit <- function(x) {
-  exp(x) / sum(exp(x))
-}
-
-## extract contrasts indicating whether most membership
-## comes from one topic
-beta_probs <- beta_summary %>%
-  group_by(topic) %>%
-  mutate(prob = inv_logit(beta_median)) %>%
-  select(rsv_ix, rsv, Taxon_5, prob) %>%
-  spread(topic, prob)
-
-p_topic <- beta_probs %>%
-  select(starts_with("Topic")) %>%
-  as.matrix()
-rownames(p_topic) <- beta_probs$rsv
-
-for (k in seq_len(stan_data$K)) {
-  contrast <- rep(-1, stan_data$K)
-  contrast[k] <- 1
-  beta_probs[, sprintf("topic_%s_diff", k)] <- p_topic %*% contrast %>%
-    as.numeric()
-}
-
-## join taxa, sample, and count data, for some plots
-sam_data <- abt %>%
-  sample_data() %>%
-  data.frame() %>%
-  rownames_to_column("sample") %>%
-  as_data_frame()
-
-mabt <- get_taxa(abt) %>%
-  melt(varnames = c("rsv", "sample")) %>%
-  as_data_frame() %>%
-  left_join(sam_data) %>%
-  left_join(beta_summary %>% select(rsv, Taxon_5)) %>%
-  mutate(prototypical = NA)
-
-## plot individual species that are representative
-## ("prototypical") of given topics
-prototypes <- list()
-for (k in seq_len(stan_data$K)) {
-  prototypes[[k]] <- beta_probs %>%
-    arrange_(sprintf("desc(topic_%s_diff)", k)) %>%
-    .[["rsv"]]
-  mabt[mabt$rsv %in% prototypes[[k]][1:50], "prototypical"] <- paste0("Topic ", k)
-}
-
-p <- ggplot(mabt %>% filter(!is.na(prototypical))) +
-  geom_line(
-    aes(x = time, y = value, group = rsv, col = Taxon_5),
-    alpha = 0.6
-  ) +
-  scale_y_sqrt(
-    "abundance (sqrt scale)",
-    breaks = scales::pretty_breaks(3)
-  ) +
-  scale_color_manual(
-    "Family",
-    values = c("#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3", "#a6d854", "grey"),
-    guide = guide_legend(override.aes = list(alpha = 1, size = 3)),
-    na.value = "black"
-  ) +
-  facet_grid(Taxon_5 ~ prototypical, scale = "free_y") +
-  theme(
-    strip.text.y = element_blank(),
-    panel.border = element_rect(fill = "transparent", size = 0.75),
-    legend.position = "bottom"
-  )
-ggsave(
-  sprintf("../../doc/figure/topic_prototypes-%K.png", argv$K)
-  p, width = 4.5, height = 2.8
-)
-
-for (k in seq_len(stan_data$K)) {
-  cur_prototypes <- mabt %>%
-    filter(rsv %in% prototypes[[k]][1:15])
-  cur_prototypes$rsv <- factor(cur_prototypes$rsv, levels = prototypes[[k]][1:15])
-  p <- ggplot(cur_prototypes) +
-    geom_line(aes(x = time, y = value, col = Taxon_5)) +
-    scale_y_sqrt(breaks = scales::pretty_breaks(3)) +
-    scale_color_manual(
-      "Family",
-      values = c("Lachnospiraceae" = "#66C2A5", "Ruminococcaceae" = "#FC8D62",
-                 "Bacteroidaceae" = "#8DA0CB", "uncultured" = "#E78AC3",
-                 "other" = "grey"),
-      guide = guide_legend(override.aes = list(alpha = 1, size = 3)),
-      na.value = "black"
-    ) +
-    facet_wrap(~rsv, scale = "free_y", nrow = 3) +
-    theme(
-      panel.border = element_rect(fill = "transparent", size = 0.75),
-      legend.position = "bottom"
-    )
-  ggsave(
-    sprintf("../../doc/figure/species_prototypes-%s-%s.png", k, argv$K),
-    p, width = 6.5, height = 3.5
-  )
-}
-
-## make comparisons at the family level, by studying
-## the average probability assigned to each topic,
-## across all species within that family.
-taxa_df <- tax_table(abt) %>%
-  data.frame() %>%
-  rownames_to_column("rsv")
-ave_probs <- beta_summary %>%
-  group_by(topic) %>%
-  mutate(prob = inv_logit(beta_median)) %>%
-  select(-Taxon_5) %>%
-  left_join(taxa_df) %>%
-  filter(Taxon_5 != "") %>%
-  group_by(topic, Taxon_5) %>%
-  summarise(n_prob = n(), ave_prob = mean(prob)) %>%
-  arrange(topic, desc(ave_prob))
-
-uneven_probs <- ave_probs %>%
-  filter(ave_prob > 0.0001)
-mbt_uneven_taxa <- mabt %>%
-  select(-Taxon_5) %>%
-  left_join(taxa_df) %>%
-  filter(Taxon_5 %in% uneven_probs$Taxon_5)
-
-Taxon_5_order <- uneven_probs %>%
-  group_by(Taxon_5) %>%
-  summarise(max_ave = max(ave_prob)) %>%
-  arrange(desc(max_ave)) %>%
-  .[["Taxon_5"]]
-uneven_probs$Taxon_5 <- factor(
-  uneven_probs$Taxon_5,
-  levels = Taxon_5_order
-)
-
-## visualize the unevenness in topic assignments across taxa
-p <- ggplot(uneven_probs) +
-  geom_point(
-    aes(
-      x = Taxon_5,
-      y = ave_prob,
-      col = topic,
-      size = n_prob
-    ),
-    alpha = 0.6
-  ) +
-  labs(x = "Family", y = "Average Topic Probability", size = "n[taxa]") +
-  theme(
-    axis.text.x = element_text(angle = -90, hjust = 0),
-    panel.border = element_rect(size = 1, fill = "transparent")
-  )
-ggsave(
-  sprintf("../../doc/figure/uneven_taxa_ordered-%s.png", argv$K),
-  p, width = 5, height = 5
-)
-
-mbt_uneven_taxa$Taxon_5 <- factor(mbt_uneven_taxa$Taxon_5, levels = Taxon_5_order)
-p <- ggplot(mbt_uneven_taxa) +
-  geom_line(
-    aes(x = time, y = value, group = rsv, col = prototypical),
-    alpha = 0.6
-  ) +
-  scale_y_sqrt(breaks = scales::pretty_breaks(3)) +
-  scale_color_hue("Prototype", guide = guide_legend(override.aes = list(alpha = 1, size = 2))) +
-  facet_wrap(~ Taxon_5, scale = "free_y") +
-  labs(y = "abundance (sqrt scale)") +
-  theme(
-    strip.text = element_text(size = 6.5),
-    axis.text = element_text(size = 6),
-    legend.position = "bottom",
-    panel.border = element_rect(fill = "transparent", size = 0.75)
-  )
-ggsave(
-  sprintf("../../doc/figure/uneven_taxa_facet.png", argv$K),
-  p, width = 6, height = 3.5
-)
+plot(asinh(x_out[, 7]))
 
 ## ---- posterior-checks ----
 checks_data <- posterior_checks_input(
